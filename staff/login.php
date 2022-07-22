c2562c20a3a9f9b4bb78f9e420d7a801f492b6e9
FahmiEfendy <fahmi.efendy@binus.ac.id>
2022-07-22 10:30:14 +0700

    feat add styling for staff login page

--- staff/login.php
+++ staff/login.php
@@ -57,38 +57,53 @@ if(isset($_POST['login'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Staff Login</title>
 
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- External CSS -->
     <link rel="stylesheet" href="../CSS/style.css">
-
 </head>
-<body>
+<body class="bodyStyling">
     
-    <h1>Staff Login</h1>
+    <div class="container w-25 text-center containerStyling">
 
-    <?php if(isset($errorMsg)) : ?>
-        <p>email/password salah!</p>
-    <?php endif; ?>
-    
-    <form action="" method="post">
-        <ul>
-            <li>
-                <label for="email">Email</label>
-                <input type="text" name="email" id="email" required>
-            </li>
-            <li>
-                <label for="password">Password</label>
-                <input type="password" name="password" id="password" required>
-            </li>
-            <li>
-                <button type="submit" name="login">Login</button>
-            </li>
-            <li>
-                <a href="">Login as Superadmin</a>
-            </li>
-            <li>
-                <a href="register.php">Staff Register</a>
-            </li>
-        </ul>
-    </form>
+        <h1 class="pt-5 pb-4">Staff Login</h1>
+
+        <?php if(isset($errorMsg)) : ?>
+            <div class="alert alert-danger w-50 m-auto" role="alert">
+                <h6>Email / Password Salah!</h6>
+            </div>        
+        <?php endif; ?>
+        
+        <form action="" method="post">
+            <div>
+                <div class="text-left">
+                    <label class="h5 mx-4 mb-2" for="email" >Email</label>
+                </div>
+                <div class="input-group mb-3 px-4">
+                    <input type="text" class="form-control" name="email" id="email" required>
+                    <div class="input-group-append">
+                        <span class="input-group-text" id="basic-addon2">@binus.ac.id</span>
+                    </div>
+                </div>
+                <div class="text-left">                    
+                    <label class="h5 mx-4 mb-2 mt-2" for="password">Password</label>
+                </div>
+                <div class="input-group mb-3 px-4">
+                    <input type="password" class="form-control" name="password" id="password">
+                </div>
+                <div>
+                    <button class="btn btn-primary btn-lg mt-4 w-50" type="submit" name="login">Login</button>
+                </div>
+                <div>
+                    <a class="btn btn-secondary mt-3 w-50" href="">Login as Superadmin</a>
+                </div>
+                <div>
+                    <a class="btn btn-secondary mt-3 mb-5 w-50" href="register.php">Staff Register</a>
+                </div>
+            </div>
+        </form>
+    </div>
 
 </body>
 </html>
