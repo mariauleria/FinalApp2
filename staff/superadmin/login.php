2ba058abfded8581be750bc5103e8b34b8ec1188
mariauleria <mariaauleria304@gmail.com>
2022-07-26 11:48:42 +0700

    tambah web superadmin

---
+++ staff/superadmin/login.php
@@ -0,0 +1,55 @@
+<?php 
+
+session_start();
+
+if(isset($_SESSION['login']) || isset($_SESSION['login-staff'])){
+    echo"
+    <script>
+        alert('Anda tidak punya akses ke halaman ini!');
+        document.location.href = '../';
+    </script>
+    ";
+    exit;
+}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Superadmin Login</title>
+
+    <link rel="stylesheet" href="../../CSS/style.css">
+
+</head>
+<body>
+    
+    <h1>Superadmin Login</h1>
+
+    <?php if(isset($errorMsg)) : ?>
+        <p>username/password salah!</p>
+    <?php endif; ?>
+    
+    <form action="" method="post">
+        <ul>
+            <li>
+                <label for="username">username</label>
+                <input type="text" name="username" id="username" required>
+            </li>
+            <li>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" required>
+            </li>
+            <li>
+                <button type="submit" name="login">Login</button>
+            </li>
+        </ul>
+    </form>
+
+</body>
+</html>
